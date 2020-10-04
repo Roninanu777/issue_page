d0c7aedc0ab75351ed9c1d85e73af2c051c860b3
Roninanu777 <ronirajkamalpradhan1112@gmail.co>
2020-10-05 00:25:24 +0530

    importing styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+//Styles-------------------------------------------------------
+const Container = styled.div`
+    width: 50%;
+    padding: 3%;
+    background-color: red;
+`;
+//-------------------------------------------------------------
 
 function App() {
-    return <div>Hello</div>;
+    return (
+        <Container>
+            <h1>Issue list</h1>
+        </Container>
+    );
 }
 
 export default App;
