4784d9974464e28d72a712ff28cbde6ea73f7924
Roninanu777 <ronirajkamalpradhan1112@gmail.co>
2020-10-05 10:08:15 +0530

    adding heading

---
+++ client/src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    background-color: grey;
+`;
+
+export default function Heading() {
+    return <Container>Heading component</Container>;
+}
