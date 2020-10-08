1e2a85ed7ff3403ecba46c33c9b01d6f0c18c447
Roninanu777 <ronirajkamalpradhan1112@gmail.co>
2020-10-08 23:10:30 +0530

    flexbox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Heading from "./components/Heading";
+import Modal from "./components/Modal";
 
 //Styles-------------------------------------------------------
 const Container = styled.div`
-    width: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 //-------------------------------------------------------------
 
 function App() {
-    return (
-        <Container>
-            <Heading />
-        </Container>
-    );
+  const [term, setTerm] = useState("show-all");
+  const [open, setOpen] = useState(false);
+
+  let handleTerm = (term) => {
+    setTerm(term);
+  };
+
+  let handleOpen = (open) => {
+    setOpen(open);
+  };
+
+  return (
+    <Container>
+      <Heading getTerm={handleTerm} getOpen={handleOpen} />
+      <Modal />
+    </Container>
+  );
 }
 
 export default App;
