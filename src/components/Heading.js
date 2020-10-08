70de85f2db8013031fc22b65e0a3ab39968a63d7
Roninanu777 <ronirajkamalpradhan1112@gmail.co>
2020-10-08 23:09:48 +0530

    minor

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,64 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 //-----------------------------------------------------------
 
 const Container = styled.div`
-    width: 100%;
-    margin-top: 5%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 100%;
+  margin-top: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Select = styled.select`
-    padding: 1%;
-    outline: none;
-    border: 1px solid #ababab;
-    border-radius: 5px 0 0 5px;
-    background-color: #f7f7f7;
+  padding: 1%;
+  outline: none;
+  border: 1px solid #ababab;
+  border-radius: 5px 0 0 5px;
+  background-color: #f7f7f7;
 `;
 
 const Input = styled.input`
-    padding: 1.1%;
-    width: 70%;
-    border: 1px solid #ababab;
-    outline: none;
-    border-radius: 0 5px 5px 0;
+  padding: 1.1%;
+  width: 70%;
+  border: 1px solid #ababab;
+  outline: none;
+  border-radius: 0 5px 5px 0;
 `;
 
 const Button = styled.button`
-    margin-left: 20px;
-    padding: 1.1%;
-    border: none;
-    border-radius: 5px;
-    color: white;
-    outline: none;
-    cursor: pointer;
-    background-color: #159930;
+  margin-left: 20px;
+  padding: 1.1%;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  outline: none;
+  cursor: pointer;
+  background-color: #159930;
 `;
 
 //-----------------------------------------------------------
 
-export default function Heading() {
-    const [value, setValue] = useState("filters");
+export default function Heading(props) {
+  const [value, setValue] = useState("filters");
 
-    let handleChange = (e) => {
-        setValue(e.target.value);
-    };
+  let handleChange = (e) => {
+    setValue(e.target.value);
+    props.getTerm(e.target.value);
+  };
 
-    return (
-        <Container>
-            <Select value={value} onChange={handleChange}>
-                <option value="filters" hidden>
-                    Filters
-                </option>
-                <option value="show-all">Show all</option>
-                <option value="open">Open</option>
-                <option value="closed">Closed</option>
-            </Select>
-            <Input type="text" placeholder="is:issue is:open" />
-            <Button type="submit">New issue</Button>
-        </Container>
-    );
+  return (
+    <Container>
+      <Select value={value} onChange={handleChange}>
+        <option value="filters" hidden>
+          Filters
+        </option>
+        <option value="show-all">Show all</option>
+        <option value="open">Open</option>
+        <option value="closed">Closed</option>
+      </Select>
+      <Input type="text" placeholder="is:issue is:open" />
+      <Button type="button">New issue</Button>
+    </Container>
+  );
 }
