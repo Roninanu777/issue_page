60e3e76146136536757eebc6db74b77c8a62fd7b
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-10-09 15:37:09 +0530

    issue item

---
+++ src/components/IssueList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+//Styles-----------------------------------------------------
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+//-----------------------------------------------------------
+
+export default function IssueList() {
+  return <Container></Container>;
+}
